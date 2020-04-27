22b9c0db41197e099c5cd10ad1a3a719150164f7
Nicholas Marino <njm256@gmail.com>
2020-04-27 18:48:24 -0400

    trivial FEN diagram

--- position/fen.go
+++ position/fen.go
@@ -1,5 +1,10 @@
 package position
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Fen struct {
 	pos         string
 	activeColor string
@@ -7,6 +12,38 @@ type Fen struct {
 	ep          string
 	hClock      int
 	move        int
+}
+
+func NewFen(str string) *Fen {
+	s := strings.Split(str, " ")
+	newPos := new(Fen)
+	newPos.pos = s[0]
+	newPos.activeColor = s[1]
+	newPos.castling = s[2]
+	newPos.ep = s[3]
+	newPos.hClock, _ = strconv.Atoi(s[4])
+	newPos.move, _ = strconv.Atoi(s[5])
+	return newPos
+}
+
+func (f Fen) String() string {
+	ranks := strings.Split(f.pos, "/")
+	r := strings.NewReplacer(
+		"1", " ",
+		"2", "  ",
+		"3", "   ",
+		"4", "    ",
+		"5", "     ",
+		"6", "      ",
+		"7", "       ",
+		"8", "        ",
+	)
+	var newRanks [8]string
+	for i := 0; i < 8; i++ {
+		newRanks[i] = r.Replace(ranks[i])
+	}
+	return strings.Join(newRanks[:], "\n")
+
 }
 
 func Wtf() {
